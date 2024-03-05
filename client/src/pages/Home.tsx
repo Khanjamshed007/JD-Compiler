52f2092ed000951c435b5d04de6a672badf1d96d
jamshed khan <khanjamshed404@gmail.com>
2024-03-06 01:13:35 +0530

    added home ui

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,8 +1,10 @@
-import React from 'react'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className="flex justify-center text-white items-center w-full h-[calc(100dvh-60px)] flex-col">
+      <h1 className="text-6xl font-bold text-center mb-2">JD Compiler</h1>
+      <p className="text-xl text-gray-600">Compiler HTML, CSS and JavaScript Code on the go and share with your friends</p>
+    </div>
   )
 }
 
