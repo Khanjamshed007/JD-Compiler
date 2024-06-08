abd2735dc34cdb4bab03fd2595a3ca6a9f851607
jamshed khan <khanjamshed404@gmail.com>
2024-06-08 11:46:02 +0530

    new vercel data

--- server/src/index.ts
+++ server/src/index.ts
@@ -4,51 +4,29 @@ import { config } from "dotenv";
 import { dbConnect } from "./lib/dbConnect";
 import { CompilerRouter } from "./Routes/CompilerRoutes";
 import { UserRoutes } from "./Routes/UserRoutes";
-
 const app = express();
 
 app.use(express.json());
 
 const corsOptions = {
   origin: "https://jd-compiler-client.vercel.app",
-  methods: "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS",
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
   allowedHeaders: ["Content-Type", "Authorization"],
-  optionsSuccessStatus: 204,
+  Credential: true,
 };
 
 app.use(cors(corsOptions));
-
-// Additional middleware to handle preflight requests
-app.options("*", cors(corsOptions));
-
 config();
-
-app.use((req, res, next) => {
-  console.log(`${req.method} request for '${req.url}'`);
-  next();
-});
-
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://jd-compiler-client.vercel.app');
-  res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  next();
-});
-
-app.options('*', (req, res) => {
-  res.header('Access-Control-Allow-Origin', 'https://jd-compiler-client.vercel.app');
-  res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  res.sendStatus(204);
-});
-
-
 
 app.use("/compiler", CompilerRouter);
 app.use("/user", UserRoutes);
 
 dbConnect();
 
+app.get("/", (req, res) => {
+  res.json("hello buddy");
+});
+
 app.listen(4000, () => {
-  console.log("Server running at http://localhost:4000");
+  console.log("http://localhost:4000");
 });
