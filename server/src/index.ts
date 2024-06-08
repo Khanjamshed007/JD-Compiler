37f1fddc3deb8f527a0339cc0b7b162ead2c4d0a
jamshed khan <khanjamshed404@gmail.com>
2024-06-08 11:35:23 +0530

    cors changes

--- server/src/index.ts
+++ server/src/index.ts
@@ -23,6 +23,27 @@ app.options("*", cors(corsOptions));
 
 config();
 
+app.use((req, res, next) => {
+  console.log(`${req.method} request for '${req.url}'`);
+  next();
+});
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'https://jd-compiler-client.vercel.app');
+  res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
+
+app.options('*', (req, res) => {
+  res.header('Access-Control-Allow-Origin', 'https://jd-compiler-client.vercel.app');
+  res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.sendStatus(204);
+});
+
+
+
 app.use("/compiler", CompilerRouter);
 app.use("/user", UserRoutes);
 
